6e3fccba6b99ff74586ad35c0bcd30688e0fc0c1
terrytao <279090940@qq.com>
2015-08-11 01:22:03 +0800

    adding

---
+++ src/main/java/com/tl/shop/controllers/FileUploadController.java
@@ -0,0 +1,17 @@
+package com.tl.shop.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+@Controller
+public class FileUploadController {
+
+	@RequestMapping(value = "/upload", method = RequestMethod.POST)
+	public String upload(@RequestParam("file") MultipartFile file) {
+		System.out.println(file.getContentType());
+		return "shop/home";
+	}
+}
