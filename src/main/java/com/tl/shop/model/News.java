9e275f25bf2052d5a5388bff891b38928d52280d
terrytao <taoli51@gmail.com>
2015-08-25 16:58:28 +0800

    starting developing for detail page

---
+++ src/main/java/com/tl/shop/model/News.java
@@ -0,0 +1,32 @@
+package com.tl.shop.model;
+
+public class News {
+	private String title;
+	private String contents;
+	private String imageURL;
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getContents() {
+		return contents;
+	}
+
+	public void setContents(String contents) {
+		this.contents = contents;
+	}
+
+	public String getImageURL() {
+		return imageURL;
+	}
+
+	public void setImageURL(String imageURL) {
+		this.imageURL = imageURL;
+	}
+
+}
